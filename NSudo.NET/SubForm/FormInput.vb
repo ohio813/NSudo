3603f683bfd0ae1d41b915ae63db82f4338f0fd7
杨汝清 <visualstudio2014@live.com>
2015-10-31 20:46:31 +0800

    换回

--- NSudo.NET/SubForm/FormInput.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class FormInput
-    Public Info As InfoText = New InfoText(RichTextBoxMessage)
-    Private Sub FormInput_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Info.Show("应用程序正常初始化...")
-        Info.Show("操作系统:" & My.Computer.Info.OSFullName)
-        Info.Show("是否64位进程:" & Environment.Is64BitProcess)
-    End Sub
-End Class
